e5d0ff663819d92d06e83fe5b77ee21f7e466732
iram50 <iramalex01@gmail.com>
2023-11-01 11:29:49 -0700

    se agrego favorito como placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,14 @@ class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex=0;
   @override
   Widget build(BuildContext context){
+    Widget page;
+    switch(selectedIndex){
+      case 0: page = GeneratorPage(); break;
+      case 1: page = Placeholder(); break;
+      default:
+      throw UnimplementedError('No hay un widget para: $selectedIndex');
+    }
+
     return Scaffold(
       body: Row(
         children: [
@@ -83,7 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page,
             ))
       ],)
     );
