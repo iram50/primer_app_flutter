08403b0e72e9de2014be9fcb5c18da9acef20bd6
iram50 <iramalex01@gmail.com>
2023-11-01 11:57:06 -0700

    se agrego el favoritos por lo menos su primera version

--- lib/main.dart
+++ lib/main.dart
@@ -61,39 +61,43 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget page;
     switch(selectedIndex){
       case 0: page = GeneratorPage(); break;
-      case 1: page = Placeholder(); break;
+      case 1: page = FavoritosPage(); break;
       default:
       throw UnimplementedError('No hay un widget para: $selectedIndex');
     }
 
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [NavigationRailDestination(
-                icon: Icon(Icons.home), 
-                label: Text("Inicio")),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                   label: Text("Favoritos"))
-                
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-            ) 
-          ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: page,
-            ))
-      ],)
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [NavigationRailDestination(
+                    icon: Icon(Icons.home), 
+                    label: Text("Inicio")),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                       label: Text("Favoritos"))
+                    
+                    ],
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                ) 
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ))
+          ],)
+        );
+      }
     );
   }
 }
@@ -171,5 +175,27 @@ class GeneratorPage extends StatelessWidget{
 
 }
 
+class FavoritosPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var appState= context.watch<MyAppState>();
 
+    if (appState.favoritos.isEmpty){
+      return Center(child: Text("Aun no hay favoritos"),);
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text("Se han elegido ${appState.favoritos.length} favoritos"),),
+          for (var name in appState.favoritos)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(name.asLowerCase),
+          )
+      ],
+      );
+  }
+}
   
